19a73443117f680951c0c0b96ed4b457a055e896
sirreal <jon.surrell@gmail.com>
2014-12-16 18:28:11 +0100

    cleanup messy js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 var pd = require('pretty-data').pd
-  , concat = require('concat-stream')
-  , prettyxml = require('concat-stream');
+  , concat = require('concat-stream');
 
-var prettify = concat({'encoding': 'string'}, function(d) {
+var prettyxml = concat({'encoding': 'string'}, function(d) {
   process.stdout.write(pd.xml(d));
 });
 
-process.stdin.pipe(prettify);
+process.stdin.pipe(prettyxml);
