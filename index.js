5ab7efe9307c214d824d4927883e4b6b6eac907c
Jon Surrell <jon.surrell@gmail.com>
2015-06-15 17:02:40 +0200

    uniform format

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-var pd = require('pretty-data').pd
-  , concat = require('concat-stream');
+var pd = require('pretty-data').pd;
+var concat = require('concat-stream');
 
 var prettyxml = concat({'encoding': 'string'}, function(d) {
   process.stdout.write(pd.xml(d));
 });
 
 process.stdout.on('error', function(err){
-	// been piped to a process that has closed stdout. e.g. more
-	if (err.code === "EPIPE") {
-		process.exit(0);
-	}
+  // been piped to a process that has closed stdout. e.g. more
+  if (err.code === "EPIPE") {
+    process.exit(0);
+  }
 
   // something else, don't mask error
-	process.stderr.write(JSON.stringify(err) + '\n');
-	process.exit(1);
+  process.stderr.write(JSON.stringify(err) + '\n');
+  process.exit(1);
 });
 
 process.stdin.pipe(prettyxml);
